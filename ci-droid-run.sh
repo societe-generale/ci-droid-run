a109dfe9cee7967651039f5e17ed60c10adde22f
Vincent Fuchs <vincent.fuchs@gmail.com>
2018-08-04 16:09:32 +0530

    images get built properly now

--- ci-droid-run.sh
+++ ci-droid-run.sh
@@ -12,7 +12,7 @@ docker build . -f Dockerfile-ci-droid --build-arg APPLI_NAME=$APPLI_NAME -t $APP
 
 echo "downloading CI-Droid tasks consumer part.."
 APPLI_NAME=ci-droid-tasks-consumer
-curl http://repo1.maven.org/maven2/com/societegenerale/ci-droid/ci-droid-starter/$CI_DROID_REST_VERSION/ci-droid-starter-$CI_DROID_REST_VERSION-exec.jar --insecure -o $APPLI_NAME.jar
+curl http://repo1.maven.org/maven2/com/societegenerale/ci-droid/tasks-consumer/ci-droid-tasks-consumer-starter/$CI_DROID_TASKS_CONSUMER_VERSION/ci-droid-tasks-consumer-starter-$CI_DROID_TASKS_CONSUMER_VERSION-exec.jar --insecure -o $APPLI_NAME.jar
 echo "building $APPLI_NAME image.."
 docker build . -f Dockerfile-ci-droid --build-arg APPLI_NAME=$APPLI_NAME -t $APPLI_NAME
 
